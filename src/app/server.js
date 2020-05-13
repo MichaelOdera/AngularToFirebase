fbfcd2aea4342c465f625a06a1a4a4c78f6b212b
MichaelOdera <michogelira@gmail.com>
2020-05-14 01:42:10 +0300

    changed name in server.js from goals to Goals

--- src/app/server.js
+++ src/app/server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/goals'));
+app.use(express.static(__dirname + '/dist/Goals'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/goals/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/Goals/index.html'));
 });
 
 // default Heroku PORT
