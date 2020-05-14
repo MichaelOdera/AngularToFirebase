1c3547b1ea8a82c2f4ec794d5185f81453702b95
MichaelOdera <michogelira@gmail.com>
2020-05-14 21:34:52 +0300

    added an alert service to show message when a deletion has happened

---
+++ src/app/goal-service/goal.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { Goal } from '../goal';
+import { from } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GoalService {
+
+  goals: Goal[] = [
+    new Goal(1, 'Finding Nemo', 'Watching a tutorial clip', new Date(2019, 3, 14)),
+    new Goal(2, 'Breaking bread', 'The upper room experience', new Date(2020, 2, 16)),
+    new Goal(3, 'Seeking for refuge', 'The place of safety is to be found', new Date(2020, 5, 11)),
+    new Goal(4, 'Learn that grace is Sufficient', 'Watching for the strength that is greater than I', new Date(2020, 8, 20))
+  ];
+
+
+  getGoals(){
+    return this.goals;
+  }
+
+  constructor() { }
+}
