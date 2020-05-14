2200b48448dbf500fe9a56b69434ed15aa6a3eb4
MichaelOdera <michogelira@gmail.com>
2020-05-14 21:58:47 +0300

    resolved the baseUrl error

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  baseUrl: "http://quotes.stormconsultancy.co.uk/random.json"
+  baseUrl: "http://quotes.stormconsultancy.co.uk/random.json",
+  production: false
 };
 
 /*
