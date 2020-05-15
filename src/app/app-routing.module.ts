6a7e5534017b2e0fdc2c3301acb17d31c0da4213
MichaelOdera <michogelira@gmail.com>
2020-05-15 18:16:43 +0300

    added the various paths to the components in the app-routing.module.ts file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GoalComponent } from './goal/goal.component';
+import { AboutComponent } from './about/about.component';
+import { GoogleSearchComponent } from './google-search/google-search.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { GoalDetailComponent } from './goal-detail/goal-detail.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:"/goals", pathMatch:"full"},
+  { path: 'goals', component: GoalComponent},
+  { path: 'goals/:id', component: GoalDetailComponent},
+  { path: 'about', component: AboutComponent},
+  { path: 'search', component: GoogleSearchComponent},
+  { path:'**', component: NotfoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
