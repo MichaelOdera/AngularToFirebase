51223275bde668f00cde4b7841be3df7b1f07496
MichaelOdera <michogelira@gmail.com>
2020-05-21 04:14:40 +0300

    added the necessary https format on environment.ts to allow for fetching of the api data

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: "http://quotes.stormconsultancy.co.uk/random.json",
+  baseUrl: "https://quotes.stormconsultancy.co.uk/random.json",
 };
